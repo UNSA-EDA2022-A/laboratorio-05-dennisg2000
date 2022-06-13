c9a8d9e87bb4f6001496a482f2d4d8a396b8c058
dennisg2000 <84654444+dennisg2000@users.noreply.github.com>
2022-06-12 21:36:07 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -14,8 +14,54 @@ public class Exercise2 {
 
     public boolean existenDuplicados(String str) {
         MyStack<Character> stack = new LinkedListStack<>();
+        MyStack<Character> stack2 = new LinkedListStack<>();
         // Colocar codigo aqui
+        for(int i=0;i<str.length();i++) {
+        	char a=str.charAt(i);
+        	if(a=='(' || a==')' || a=='+' || a=='-') {
+        		stack.push(a);
+        	}
+        	
+        }
+        boolean es=true,es2=true,esab=false;;
+        int size=stack.size();
+        int ab=0,cer=0,op1=0,op2=0;
+        for(int i=0;i<size;i++) {
+    		char temp =stack.pop();
+    		stack2.push(temp);
+    		if(temp=='(') {
+    			cer+=1;
+    		}
+    		if(temp=='+' || temp=='-') {
+    			op1+=1;
+    		}
+    			
+    		if(cer<=op1 ) {
+    			es=false;
+    		}else {
+    			es = true;
+    			break;
+    		}
+    	}
+        for(int i=0;i<size;i++) {
+    		char temp =stack2.pop();
+    		if(temp==')') {
+    			ab+=1;
+    		}
+    			
+    		if(temp=='+' || temp=='-') {
+    			op2+=1;
+    		}
+    			
+    		if(ab<=op2 ) {
+    			es2=false;
+    		}else {
+    			es2 = true;
+    			break;
+    		}
+        }
+        System.out.println(stack);
 
-        return false;
+        return es || es2;
     }
 }
